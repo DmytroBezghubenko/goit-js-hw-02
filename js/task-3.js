b969b728fd37aa8ac47c58fbefc0f494e68fd363
DmytroBezghubenko <dimon14032@gmail.com>
2024-01-29 20:10:37 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function checkForSpam(message) {
-     return message.toLowerCase().includes('spam' || 'sale');
+     return message.toLowerCase().includes('spam')  ||  message.toLowerCase().includes('sale');
 }
 
 console.log(checkForSpam("Latest technology news")); // false
